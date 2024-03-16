510a642a0dc685f44f2100913f4c0d7fc30eba69
TalaatGamal <you@example.com>
2024-03-16 02:06:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// comment
+
+    alert("Please note that this website is still under development by Engineer Talaat and that it is not the final version يرجى العلم أن هذا الموقع لا يزال قيد التطوير وأنه ليس النسخة النهائية");
+
+    document.write("Dev Talaat Gamal")
+
